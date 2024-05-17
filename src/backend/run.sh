3422b5b61391953ea18972a310e4d870ad4de904
Stephen A. Fuqua <stephen@safnet.com>
2024-05-17 15:08:34 -0500

    Add header Working install script

--- src/backend/run.sh
+++ src/backend/run.sh
@@ -1,10 +1,32 @@
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -p 5432 -U $POSTGRES_USER -c '\q';
-do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 10
-done
->&2 echo "Postgres is up - executing command"
-exec $cmd
-# Run the .NET application
-dotnet /app/installer/EdFi.DataManagementService.Backend.Installer.dll postgresql "host=localhost;port=5432;username=postgres;password=password;database=EdFi.DataManagementService;"
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
 
+# Use this script for building a PostgreSQL container that is pre-loaded
+# with the Data Management Service schema.
+
+if [ ! -s "$PGDATA/PG_VERSION" ]; then
+  echo "Initializing PostgreSQL database directory..."
+  initdb $PGDATA
+fi
+
+[ $? -ne 0 ] && exit || echo "Starting PostgreSQL service..."
+
+pg_ctl -D "$PGDATA" -o "-c listen_addresses='*'" -w start
+
+[ $? -ne 0 ] && exit || echo "Waiting for PostgreSQL to start..."
+
+until pg_isready -h localhost -p 5432 -U postgres; do
+  echo "Waiting for PostgreSQL to start..."
+  sleep 2
+done
+
+echo "PostgreSQL is ready. Install Data Management Service schema."
+
+/app/EdFi.DataManagementService.Backend.Installer \
+    postgresql \
+    "host=localhost;port=5432;username=postgres;database=edfi_dms;"
+
+echo "Shutdown PostgreSQL"
+pg_ctl -D $(psql -Xtc 'show data_directory') stop
